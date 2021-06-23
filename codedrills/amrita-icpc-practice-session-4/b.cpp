133251465de0807596eb5dca7afe1b6c9cd3347d
Amey Bhavsar <28699912+ameybhavsar24@users.noreply.github.com>
2021-06-23 19:45:25 +0530

    solved Maximum Prefix Mex Sum

--- codedrills/amrita-icpc-practice-session-4/b.cpp
+++ codedrills/amrita-icpc-practice-session-4/b.cpp
@@ -11,12 +11,36 @@ typedef long long ll;
 #define ss second
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin, (x).rend()
-#define MOD 1000000007
+const ll MOD = 1000000007;
+const ll nax = 1e5 + 5;
+vector< ll > factmods(nax + 5);
 void solve() {
-	
+		int n;
+		cin >> n;
+		map< ll, ll> mp;
+		rep(i,0,n) {
+			ll x;
+			cin >> x;
+			mp[x]++;
+		}
+		ll cnt = 0, ans = 1;
+		rep(i,0,n) {
+			if (mp[i] == 0) break;
+			ans *= mp[i];
+			ans %= MOD;
+			++cnt;
+		}
+		ans *= factmods[n - cnt];
+		ans %= MOD;
+		cout << ans << '\n';
 }
 int main() {
 	ios_base::sync_with_stdio(false);cin.tie(NULL);
+	factmods[0] = 1;
+	rep(i, 1, nax) {
+		factmods[i] = (i*factmods[i-1]) % MOD;
+	}
+	
 	int t;
 	cin >> t;
 	while (t--) {
